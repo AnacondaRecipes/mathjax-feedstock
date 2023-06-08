0f01fab685524798d0d0cacfae38a97d92311b1c
Anton Kabanovs <akabanovs@anaconda.com>
2023-06-08 16:31:23 +0100

    test for win

--- recipe/mathjax-path.bat
+++ recipe/mathjax-path.bat
@@ -1,6 +1,10 @@
 @echo off
 REM script_dir == [env_root]\Scripts\
 set "script_dir=%~dp0"
+tree %script_dir
+echo %script_dir:~0,-9%\Library\lib\mathjax\es5%
+set "test= %script_dir:~0,-9%\Library\lib\mathjax\es5%"
+tree %test
 set "mathjax=%script_dir:~0,-9%\Library\lib\mathjax\es5\"
 if exist "%mathjax%" (
     echo %mathjax%
