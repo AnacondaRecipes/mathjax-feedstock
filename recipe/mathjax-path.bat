7bb9e19e22fe98cb5cd0d8f2a9e8c5ec2595de5d
Anton Kabanovs <akabanovs@anaconda.com>
2023-06-09 13:55:44 +0100

    Test windows - revert changes in mathjax-path.bat; Do I need m2-base?

--- recipe/mathjax-path.bat
+++ recipe/mathjax-path.bat
@@ -2,9 +2,8 @@ setlocal enabledelayedexpansion
 
 REM @echo off
 REM script_dir == [env_root]\Scripts\
-REM set "script_dir=%~dp0"
-REM set "mathjax=%script_dir:~0,-9%\Library\lib\mathjax\es5\"
-set "mathjax=%LIBRARY_LIB%\mathjax\es5\"
+set "script_dir=%~dp0"
+set "mathjax=%script_dir:~0,-9%\Library\lib\mathjax\es5\"
 if exist "%mathjax%" (
     echo %mathjax%
 ) else (
