0e24edd1962fbf764d3c9c0f1f0a35563ed9a830
Anton Kabanovs <akabanovs@anaconda.com>
2023-06-12 11:35:05 +0100

    modify bld.bat script to copy files for mathjax

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,21 +2,29 @@ set "mathjax=%LIBRARY_LIB%\mathjax"
 mkdir "%mathjax%" || exit 1
 
 REM move config "%mathjax%\" || exit 1
-robocopy "%cd%\config" "%mathjax%\config" /E || exit 1
+robocopy "%cd%\config" "%mathjax%\config" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move docs "%mathjax%\" || exit 1
-robocopy "%cd%\docs" "%mathjax%\docs" /E || exit 1
+robocopy "%cd%\docs" "%mathjax%\docs" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move extensions "%mathjax%\" || exit 1
-robocopy "%cd%\extensions" "%mathjax%\extensions" /E || exit 1
+robocopy "%cd%\extensions" "%mathjax%\extensions" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move fonts "%mathjax%\" || exit 1
-robocopy "%cd%\fonts" "%mathjax%\fonts" /E || exit 1
+robocopy "%cd%\fonts" "%mathjax%\fonts" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move jax "%mathjax%\" || exit 1
-robocopy "%cd%\jax" "%mathjax%\jax" /E || exit 1
+robocopy "%cd%\jax" "%mathjax%\jax" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move localization "%mathjax%\" || exit 1
-robocopy "%cd%\localization" "%mathjax%\lacalization" /E || exit 1
+robocopy "%cd%\localization" "%mathjax%\lacalization" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move test "%mathjax%\" || exit 1
-robocopy "%cd%\test" "%mathjax%\test" /E || exit 1
+robocopy "%cd%\test" "%mathjax%\test" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 REM move unpacked "%mathjax%\" || exit 1
-robocopy "%cd%\unpacked" "%mathjax%\unpacked" /E || exit 1
+robocopy "%cd%\unpacked" "%mathjax%\unpacked" /E
+if %ERRORLEVEL% GEQ 8 exit 1
 
 del /q "*.md" ".gitignore" ".travis.yml" "bower.json" "composer.json" "latest.js" "package.json" || exit 1
 xcopy /s "%cd%" "%mathjax%" || exit 1
