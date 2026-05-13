0d467bb04c35c0a6e5994400bfb8a4f4d82a5b61
Ivan Iziumov <iiziumov@anaconda.com>
2026-05-13 16:28:40 +0200

    mathjax 4.1.2

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,18 @@ set -euo pipefail
 IFS=$'\n\t'
 
 mathjax="$PREFIX/lib/mathjax"
+bin_dir="$PREFIX/bin"
+
 mkdir -p "$mathjax"
+find . -maxdepth 1 \
+    ! -name '.' \
+    ! -name '.github' \
+    ! -name '.gitignore' \
+    ! -name 'build_env_setup.*' \
+    ! -name 'conda_build.*' \
+    ! -name 'metadata_conda_debug.yaml' \
+    -exec cp -r {} "$mathjax/" \;
 
-rm -f *.md .gitignore .travis.yml bower.json composer.json latest.js package.json metadata_conda_debug.yaml
-cp -r . "$mathjax/"
-rm $mathjax/*.sh "$mathjax/LICENSE"
-
-mkdir -p "$PREFIX/bin"
-cp "${RECIPE_DIR}/mathjax-path" "$PREFIX/bin/"
-chmod +x "$PREFIX/bin/mathjax-path"
-
+mkdir -p "${bin_dir}"
+cp "${RECIPE_DIR}/mathjax-path" "${bin_dir}/"
+chmod +x "${bin_dir}/mathjax-path"
